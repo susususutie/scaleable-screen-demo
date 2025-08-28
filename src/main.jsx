d0d30e8f81f69da9f7cede7162ab358a6e910166
sutie <susususutie@gmail.com>
2025-08-28 16:05:48 +0800

    chore: upgrade

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { ResponsiveScale } from 'react-responsive-scale'
+import { ReactResponsiveScale } from 'react-responsive-scale'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ResponsiveScale backgroundColor='#eee'>
+    <ReactResponsiveScale backgroundColor='#eee'>
       <App />
-    </ResponsiveScale>
+    </ReactResponsiveScale>
   </StrictMode>
 )
